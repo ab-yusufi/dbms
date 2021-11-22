a8f6c712739d0d6bac1ab49644452c0ead877681
abyusufi <abdurrahmanyusufi478@gmail.com>
2021-11-22 23:35:58 +0530

    minor bug resolved

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ mongoose
     console.log("DB CONNECTED");
   });
 
-  // app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 
 const PORT = process.env.PORT || 5000;
 
