069d024427ecaa7107aa65a918f2f3e072964c95
abyusufi <abdurrahmanyusufi478@gmail.com>
2021-11-22 23:35:12 +0530

    added home

--- client/src/auth/Home.js
+++ client/src/auth/Home.js
@@ -1,11 +1,274 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
 const Home = () => {
-    return (
-        <div>
-          Welcome To Our Website  
+  return (
+    <div className="u-body">
+      <header className="u-clearfix u-header u-header" id="sec-5a48">
+        <div className="u-clearfix u-sheet u-sheet-1">
+          <nav className="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+            <div
+              className="menu-collapse"
+              style={{fontSize: "1rem", letterSpacing: "0px"}}
+            >
+              <a
+                className="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
+                href="#"
+              ></a>
+            </div>
+            <div className="u-custom-menu u-nav-container">
+              <ul className="u-nav u-unstyled u-nav-1">
+                <li className="u-nav-item">
+                  <a
+                    className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
+                    href="Home.html"
+                    style={{padding: "10px 20px;"}}
+                  >
+                    Home
+                  </a>
+                </li>
+                <li className="u-nav-item">
+                  <a
+                    className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
+                    href="Contact.html"
+                    style={{padding: "10px 20px;"}}
+                  >
+                    Contact
+                  </a>
+                </li>
+                <li className="u-nav-item">
+                  <a
+                    className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base"
+                    style={{padding: "10px 20px;"}}
+                  >
+                    Register
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="u-custom-menu u-nav-container-collapse">
+              <div className="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+                <div className="u-inner-container-layout u-sidenav-overflow">
+                  <div className="u-menu-close"></div>
+                  <ul className="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+                    <li className="u-nav-item">
+                      <a
+                        className="u-button-style u-nav-link"
+                        href="Home.html"
+                        style={{padding: "10px 20px;"}}
+                      >
+                        Home
+                      </a>
+                    </li>
+                    <li className="u-nav-item">
+                      <a
+                        className="u-button-style u-nav-link"
+                        href="Contact.html"
+                        style={{padding: "10px 20px;"}}
+                      >
+                        Contact
+                      </a>
+                    </li>
+                    <li className="u-nav-item">
+                      <a
+                        className="u-button-style u-nav-link"
+                        style={{padding: "10px 20px;"}}
+                      >
+                        Register
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+            </div>
+          </nav>
+          <a href="https://nicepage.com" className="u-image u-logo u-image-1">
+            <img
+              src="images/default-logo.png"
+              className="u-logo-image u-logo-image-1"
+            />
+          </a>
         </div>
-    )
-}
+      </header>
+      <section className="u-align-center u-clearfix u-section-1" id="sec-a693">
+        <div className="u-clearfix u-sheet u-sheet-1">
+          <h1 className="u-text u-text-default u-text-1">
+            Expertise,Service,Integrity.We got it...
+          </h1>
+        </div>
+      </section>
+      <section className="u-clearfix u-section-2" id="sec-e89d">
+        <div className="u-clearfix u-sheet u-sheet-1">
+          <div className="u-expanded-width u-list u-list-1">
+            <div className="u-repeater u-repeater-1">
+              <div
+                className="u-container-style u-image u-list-item u-repeater-item u-shading u-image-1"
+                data-image-width="1463"
+                data-image-height="1000"
+              >
+                <div className="u-container-layout u-similar-container u-valign-bottom u-container-layout-1">
+                  <h3 className="u-text u-text-1">
+                    Exception People,Extraodinary care
+                  </h3>
+                </div>
+              </div>
+              <div
+                className="u-container-style u-image u-list-item u-repeater-item u-shading u-image-2"
+                data-image-width="1600"
+                data-image-height="1067"
+              >
+                <div className="u-container-layout u-similar-container u-valign-bottom u-container-layout-2">
+                  <h3 className="u-text u-text-2">
+                    Top notch wide range health service
+                  </h3>
+                </div>
+              </div>
+              <div
+                className="u-container-style u-image u-list-item u-repeater-item u-shading u-image-3"
+                data-image-width="1310"
+                data-image-height="769"
+              >
+                <div className="u-container-layout u-similar-container u-valign-bottom u-container-layout-3">
+                  <h3 className="u-text u-text-3">
+                    High className authorized hospitals{" "}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="u-align-center u-clearfix u-section-3" id="sec-948f">
+        <div className="u-clearfix u-sheet u-valign-middle u-sheet-1">
+          <div className="u-container-style u-group u-group-1">
+            <div className="u-container-layout u-container-layout-1">
+              <h2 className="u-text u-text-default u-text-1">About Us</h2>
+              <p className="u-text u-text-2">
+                Logo is a super conveniet and effective website to keep a track
+                on your medical records.Logo helps you to book any health
+                service you need from blood supply to any advance medical
+                treatment with just one touch.We assure you to provide first
+                className service from authorized hospitals.We also helps you to
+                renew any of your service required at any time which make logo
+                more user friendly.
+                <br />
+                Logo not only provides user to use hospital service but gives
+                hospitals the opportunity to add any newly avaliable service or
+                details about already existing service.This helps to maintain a
+                transperency between the you and hospital which is our atmost
+                priority.
+                <br />
+                <br />
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="u-align-center u-clearfix u-section-4" id="sec-5cc3">
+        <div className="u-clearfix u-sheet u-valign-middle u-sheet-1">
+          <h4 className="u-text u-text-default u-text-1">Ask us anything</h4>
+          <div className="u-form u-form-1">
+            <form
+              action="#"
+              method="POST"
+              className="u-clearfix u-form-horizontal u-form-spacing-15 u-inner-form"
+              style={{padding: "15px"}}
+              source="custom"
+            >
+              <div className="u-form-group u-form-name u-form-group-1">
+                <label for="name-558c" className="u-form-control-hidden u-label">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  id="name-558c"
+                  name="name"
+                  className="u-border-1 u-border-grey-30 u-input u-input-rectangle"
+                  required=""
+                />
+              </div>
+              <div className="u-form-email u-form-group u-form-group-2">
+                <label for="email-558c" className="u-form-control-hidden u-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  id="email-558c"
+                  name="email"
+                  className="u-border-1 u-border-grey-30 u-input u-input-rectangle"
+                  required=""
+                />
+              </div>
+              <div className="u-form-group u-form-submit u-form-group-3">
+                <a href="#" className="u-btn u-btn-submit u-button-style">
+                  Submit
+                </a>
+                <input
+                  type="submit"
+                  value="submit"
+                  className="u-form-control-hidden"
+                />
+              </div>
+              <div className="u-form-send-message u-form-send-success">
+                Thank you! Your message has been sent.
+              </div>
+              <div className="u-form-send-error u-form-send-message">
+                Unable to send your message. Please fix errors then try again.
+              </div>
+              <input type="hidden" value="" name="recaptchaResponse" />
+            </form>
+          </div>
+        </div>
+      </section>
+      <section className="u-align-center u-clearfix u-section-5" id="sec-26e8">
+        <div className="u-align-left u-clearfix u-sheet u-sheet-1"></div>
+      </section>
 
-export default Home
+      <footer className="u-clearfix u-footer u-grey-80" id="sec-2164">
+        <div className="u-clearfix u-sheet u-sheet-1">
+          <div className="u-align-left u-social-icons u-spacing-10 u-social-icons-1">
+            <a
+              className="u-social-url"
+              title="facebook"
+              target="_blank"
+              href=""
+            ></a>
+            <a className="u-social-url" title="twitter" target="_blank" href=""></a>
+            <a
+              className="u-social-url"
+              title="instagram"
+              target="_blank"
+              href=""
+            ></a>
+            <a
+              className="u-social-url"
+              title="linkedin"
+              target="_blank"
+              href=""
+            ></a>
+          </div>
+        </div>
+      </footer>
+      <section className="u-backlink u-clearfix u-grey-80">
+        <a
+          className="u-link"
+          href="https://nicepage.com/website-templates"
+          target="_blank"
+        >
+          <span>Website Templates</span>
+        </a>
+        <p className="u-text">
+          <span>created with</span>
+        </p>
+        <a className="u-link" href="" target="_blank">
+          <span>Website Builder Software</span>
+        </a>
+        .
+      </section>
+    </div>
+  );
+};
+
+export default Home;
